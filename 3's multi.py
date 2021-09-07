d2b93a15a6736ae67d1f256f00fe975e7073e77a
pradnya1306 <90262349+pradnya1306@users.noreply.github.com>
2021-09-07 19:18:37 +0530

    Add files via upload

---
+++ 3's multi.py
@@ -0,0 +1,4 @@
+for i in range (891,932):
+    z=i-890
+    if z%3==0:
+        print(z)
---
+++ user input & sum.py
@@ -0,0 +1,5 @@
+sum=0
+for i in range(1,11):
+    num=int (input("enter the number"))
+    sum=sum+num
+print(sum)
